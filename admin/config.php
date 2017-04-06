792e6395ec4e7c628a13177ce871311bc324916d
vandanau <vandana.upreti@gmail.com>
2017-04-07 01:47:39 +0530

    inital commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shopeeking/admin/');
+define('HTTP_CATALOG', 'http://localhost/shopeeking/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shopeeking/admin/');
+define('HTTPS_CATALOG', 'http://localhost/shopeeking/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/shopeeking/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/shopeeking/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/shopeeking/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/shopeeking/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/shopeeking/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/shopeeking/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/shopeeking/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/shopeeking/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/shopeeking/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/shopeeking/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/shopeeking/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/shopeeking/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shopeeking');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shopeeking/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shopeeking/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/shopeeking/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/shopeeking/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/shopeeking/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/shopeeking/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/shopeeking/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/shopeeking/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/shopeeking/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/shopeeking/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/shopeeking/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/shopeeking/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/shopeeking/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shopeeking');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
